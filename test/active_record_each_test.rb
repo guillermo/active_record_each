2b29a427901b186f7c869327636e7c5517e9dcee
Guillermo <guillermo@humedo.cientifico.net>
2008-05-17 18:53:20 +0200

    Tests added

--- test/active_record_each_test.rb
+++ test/active_record_each_test.rb
@@ -1,8 +1,34 @@
+require 'rubygems'
+require 'activerecord'
+require File.dirname(__FILE__) + '/../lib/active_record_each.rb'
 require 'test/unit'
 
+ActiveRecord::Base.establish_connection(:adapter => "sqlite3", :database => ":memory:")
+ActiveRecord::Migration.create_table :users do |t| ; t.string :name ; end
+
+class User < ActiveRecord::Base
+end
+
 class ActiveRecordEachTest < Test::Unit::TestCase
-  # Replace this with your real tests.
-  def test_this_plugin
-    flunk
+  def setup
+    User.delete_all
+    User.create( :name => "Guillermo")
+    User.create( :name => "Esther")
+  end
+  
+  def test_each_method
+    assert !User.respond_to?(:other_strange_method_that_dont_exists)
+    assert User.respond_to?(:each)
+
+  end
+  
+  def test_map_method
+    assert User.respond_to?(:map)
+    assert User.respond_to?(:collect)
+    assert_equal User.map() {|u| u.name }.sort, ["Guillermo","Esther"].sort
+  end
+
+  def test_map_method_with_conditions
+    assert_equal User.collect(:conditions => "users.name LIKE 'G%'") {|u| u.name}, ["Guillermo"]
   end
 end

dd95bf9b19c4d79aae77766ba2071cc53b3e8d1e
Guillermo <guillermo@humedo.cientifico.net>
2008-05-17 18:53:14 +0200

    Added map and collect methods

--- lib/active_record_each.rb
+++ lib/active_record_each.rb
@@ -8,8 +8,22 @@ class ActiveRecord::Base
       set_readonly_option!(options)
       
       count(options).times do |i|
-        yield find_initial(options.merge({:offset => i}))
+        yield(find_initial(options.merge({:offset => i})))
       end
     end
+
+    def map(*args)
+      options = args.extract_options!
+      validate_find_options(options)
+      set_readonly_option!(options)
+
+      results = Array.new
+      count(options).times do |i|
+        results << yield(find_initial(options.merge({:offset => i})))
+      end
+      results
+    end
+    alias_method :collect, :map
+
   end
 end

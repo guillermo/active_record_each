742dbfef78aa590e756c4c68ff1513e18a41a1f7
Guillermo <guillermo@humedo.cientifico.net>
2008-05-17 19:02:50 +0200

    Adding rdoc documentation

--- lib/active_record_each.rb
+++ lib/active_record_each.rb
@@ -2,7 +2,18 @@
 
 class ActiveRecord::Base
   class << self
+    # Each goes througt all entries in a model, without loading all
+    # You can pass to it any parameter like find. See find for options you can pass
+    #
+    # ==== Examples:
+    #
+    #   # Goes throught all users wich name starts by g
+    #   User.each (:conditions => "users.name LIKE 'g%'") do |u|
+    #     puts u.name
+    #   end
+    #
     def each (*args)
+      return 
       options = args.extract_options!
       validate_find_options(options)
       set_readonly_option!(options)
@@ -12,6 +23,13 @@ class ActiveRecord::Base
       end
     end
 
+    # Invokes block once for each element of self. Creates a new array containing the values returned by the block
+    #
+    # ==== Example:
+    #
+    #   #Get password from md5hash
+    #   clean_password = User.map { |u| magic_recover_password(u.md5) }
+    #
     def map(*args)
       options = args.extract_options!
       validate_find_options(options)
@@ -23,6 +41,8 @@ class ActiveRecord::Base
       end
       results
     end
+
+    # Collect is an alias for map
     alias_method :collect, :map
 
   end
